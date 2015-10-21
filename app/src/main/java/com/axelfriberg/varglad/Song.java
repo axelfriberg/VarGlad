31f6a8700fdaad2b73494b740720a0b23f5a77ab
Axel Friberg <mail@axelfriberg.com>
2015-10-21 13:06:28 +0200

    Added Song class.

---
+++ app/src/main/java/com/axelfriberg/varglad/Song.java
@@ -0,0 +1,15 @@
+package com.axelfriberg.varglad;
+
+/**
+ * Represents the songs in the songbook.
+ * Created by Axel on 21/10/2015.
+ */
+public class Song {
+    String title;
+    String lyrics;
+
+    public Song(String title, String lyrics){
+        this.title = title;
+        this.lyrics = lyrics;
+    }
+}

071d070b09708ce98d4851b3c5defb49d0d04c60
Axel Friberg <mail@axelfriberg.com>
2015-10-30 16:19:08 +0100

    Added gridview with images.

--- app/src/main/java/com/axelfriberg/varglad/MainActivity.java
+++ app/src/main/java/com/axelfriberg/varglad/MainActivity.java
@@ -1,20 +1,19 @@
 package com.axelfriberg.varglad;
 
-import android.app.ListActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
 
 import java.util.List;
-import java.util.Random;
 
-public class MainActivity extends ListActivity {
+
+public class MainActivity extends Activity {
     private SongsDataSource datasource;
 
     @Override
@@ -22,23 +21,46 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+
         datasource = new SongsDataSource(this);
         datasource.open();
 
-        List<Song> values = datasource.getAllSongs();
+        String spexanthem = "Nu är det spex på gång \n " +
+                "å våran glada sång \n" +
+                " Kommer att göras över alla natten lång \n " +
+                "Kom hit så ska Ni se \n Att det blir stor succé \n " +
+                "Å vår publik den skriker om och om igen \n \n " +
+                "Nu är det spex på gång \n " +
+                "Å hela natten lång \n " +
+                "Så ska vi hålla sexa. Kom så ska vi Spexa \n " +
+                "Att Vara Glad är bäst \n " +
+                "Nu ska det bli stor fest \n " +
+                "Å vi ska festa ända fram till morgonen \n \n" +
+                "Ra-ra-raj-raj-raj...";
 
-        // use the SimpleCursorAdapter to show the
-        // elements in a ListView
-        ArrayAdapter<Song> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, values);
-        setListAdapter(adapter);
+        Song song = datasource.createSong("Spex anthem", spexanthem);
 
-        String[] comments = new String[] { "Cool", "Very nice", "Hate it" };
-        int nextInt = new Random().nextInt(3);
-        // save the new comment to the database
-        Song song = datasource.createSong(comments[nextInt],"Hello my darling, hello my bright sunshine!");
-        adapter.add(song);
-        adapter.notifyDataSetChanged();
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                List<Song> songs = datasource.getAllSongs();
+                Song song = songs.get(position);
+
+                Intent intent = new Intent(MainActivity.this, ViewSongActivity.class);
+                intent.putExtra(ViewSongActivity.EXTRA_TITLE, song.getTitle());
+                intent.putExtra(ViewSongActivity.EXTRA_LYRICS, song.getLyrics());
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
     }
 
     @Override
--- app/src/main/java/com/axelfriberg/varglad/ViewSongActivity.java
+++ app/src/main/java/com/axelfriberg/varglad/ViewSongActivity.java
@@ -1,30 +1,32 @@
 package com.axelfriberg.varglad;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class ViewSongActivity extends AppCompatActivity {
+public class ViewSongActivity extends Activity {
+    private TextView mLyricsTextView;
+    public final static String EXTRA_TITLE = "com.axelfriberg.varglad.SONG_TITLE";
+    public final static String EXTRA_LYRICS = "com.axelfriberg.varglad.SONG_LYRICS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
         setContentView(R.layout.activity_view_song);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mLyricsTextView = (TextView) findViewById(R.id.lyrics_TextView);
+        String title = intent.getStringExtra(EXTRA_TITLE);
+        String lyrics = intent.getStringExtra(EXTRA_LYRICS);
+
+        setTitle(title);
+        mLyricsTextView.setText(lyrics);
     }
-
 }
